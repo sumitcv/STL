1385ff9daf78d183c6f7baa0e8f1d7c8f051953c
Sumit Jaiswal <sumitcv@gmail.com>
2023-11-30 00:38:10 +0530

    Update stl_array.cpp

--- stl_array.cpp
+++ stl_array.cpp
@@ -13,32 +13,58 @@ Key points -
 - It does not decay to a pointer in most contexts, providing better safety compared to built-in arrays.
 */
 
-#include <iostream>
+/*
+                ~*~ ARRAY ~*~
+ */
 #include <array>
+#include <iostream>
 using namespace std;
-
 int main()
 {
-    int array_basic[3] = {1,2,3};
+    // एक array  बन गया
+    int basic[4] = {1,2,3,4} ;
     
-    // STL Array
-    array<int, 4> arr = {1,2,3,4};
-    int size = arr.size();
+    // अब देखना ये है की stl  में कैसे array  बनाया जाता है
+    // जितने element  के लिए अarray  बनाया गया है बस उतने ही element  store किए जाएँगे
+    // एक भी ज़्यादा element  डाला तो compilation  error  आएगा
+    //Excess elements in struct initializer
+    // array के साथ उसका साइज़ देना ही होगा नहीं तो error  आएगी मतलब "array<int> a" ये काम नहीं करेगा
+    array<int, 4> a  = {1,2,3,4 };
     
-    // Print || access array
-    for(int i = 0; i < size ; i++)
-        cout << arr[i] << endl;
+    // इसको प्रिंट कैसे करते है -
+    int size = a.size();
+    for(int i=0; i< size; i++)
+        cout << a[i] << endl;
+
+    // डिरेक्ट्ली कैसे ऐक्सेस कर सकते है at() -
+    cout << "print second element:  " << a.at(2) << endl;
+    cout << "print fourth  element :  " << a.at(3) << endl;
+    //array  का साइज़ हमने ४ लिया है और अगर में a.at(4) करता हूँ तो क्रश कर जाएगा।
+    //cout <<"print out of scope   :  " << a.at(3) << endl; // ये काम नहीं करेगा
     
-    // Accessed by  at funciotn operator
-    cout << arr.at(2) << endl;
+    // check the empty array ?
+    // return -> true  || false
+    cout << "-----" << a.empty() << endl;
     
-    // Empty or not
-    cout << "----" << endl;
-    cout << arr.empty() << endl;
+    // get first and last element -
+    cout << "First Element : " << a.front() << endl;
+    cout << "last element : " << a.back() << endl;
     
-    // get front & last element
-    cout << "First Element: " << arr.front() << endl;
-    cout << "Last Element: " << arr.back() << endl;
+    cout << "------------------------------------------------- " << endl;
+    
+    //array<int, 9> aa ;
+    array<int, 0> aa ;
+    cout << aa.empty() << endl;
+    cout << aa.size()<< endl;
+    
+    //returns a pointer to the first element of the array
+    cout << a.data() << endl;
+    
+    
+    // fill all element with 11.
+    a.fill(11);
+    cout << a.at(2) << endl;
     
     return 0;
 }
+
